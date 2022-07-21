35d1a206d743ef0b2195b19c5af19f6142e2cd9c
layzek <pescado@disroot.org>
2022-07-21 17:17:59 -0500

    reworked

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var box2 = document.getElementById('box2');
 var speed1 = parseInt(document.getElementById('medidor1').value); 
 var time1 = (110 / speed1) * 1000; // ms
 var seconds1 = (time1/1000).toFixed(2);
-$("#timer1").html("Tiempo 1: " + seconds1 + " segundos");
+$("#timer1").html("Tiempo que le tomaria al carro rojo a llegar al final: " + seconds1 + " segundos");
 
 var speed2 = parseInt(document.getElementById('medidor2').value); 
 var time2 = (70 / speed2) * 1000; // ms
 var seconds2 = (time2/1000).toFixed(2);
-$("#timer2").html("Tiempo 2: " + seconds2 + " segundos");
+$("#timer2").html("Tiempo que le tomaria al carro naranja a llegar al final: " + seconds2 + " segundos");
 
 function cambioMedidor1(val) {
 	document.getElementById('mostrarValor1').innerHTML = val + "m/s";
@@ -43,12 +43,12 @@ document.getElementById('play').addEventListener('click', function() {
 	var speed1 = parseInt(document.getElementById('medidor1').value); 
 	var time1 = (110 / speed1) * 1000; // ms
 	var seconds1 = (time1/1000).toFixed(2);
-	$("#timer1").html("Tiempo 1: " + seconds1 + " segundos");
+	$("#timer1").html("Tiempo que le tomaria al carro rojo a llegar al final: " + seconds1 + " segundos");
 
 	var speed2 = parseInt(document.getElementById('medidor2').value); 
 	var time2 = (70 / speed2) * 1000; // ms
 	var seconds2 = (time2/1000).toFixed(2);
-	$("#timer2").html("Tiempo 2: " + seconds2 + " segundos");
+	$("#timer2").html("Tiempo que le tomaria al carro naranja a llegar al final: " + seconds2 + " segundos");
 
 	let animation1 = box1.animate(keyframes1,{
 		duration: time1,
